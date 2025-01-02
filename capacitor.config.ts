df3c1a5c1a23f086de0e880a9c6d6b4bd8c139a6
arsenie-wallo <Arsenie.Sarmiento@wallopay.com>
2025-01-02 19:46:50 +1300

    kickstarter

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'challengeapp',
+  webDir: 'www'
+};
+
+export default config;

7ccb3889b68313616d67d6bad516b9412a6eda88
arsenie-wallo <Arsenie.Sarmiento@wallopay.com>
2025-01-10 21:35:37 -0800

    running serverless, linked to mongodb, but error on routes

--- challenge/handler.js
+++ challenge/handler.js
@@ -1,8 +1,49 @@
-exports.dashboard = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
-    }),
-  };
+require('dotenv').config();
+const mongoose = require('mongoose')
+
+// const express = require('express')
+// const cors = require('cors');
+
+// MongoDB Schema
+// Schema and Model
+const DepartmentSchema = mongoose.Schema({
+    name: String,
+    line_manager: String
+})
+
+const Department = mongoose.model('Department', DepartmentSchema);
+
+// Reuse connection for offline mode
+let isConnected;
+
+async function connectToDatabase() {
+    if (isConnected) {
+        console.log('Using existing database connection');
+        return;
+    }
+    console.log('Connecting to MongoDB...');
+    await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    isConnected = mongoose.connection.readyState;
+    console.log('Connected to MongoDB');
+}
+
+
+module.exports.dashboard = async (event) => {
+    try {
+        await connectToDatabase();
+        const departments = await Department.find();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(departments),
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to fetch items' }),
+        };
+    }
 };

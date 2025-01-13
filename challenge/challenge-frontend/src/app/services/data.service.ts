0b19121cd31084913ec8831b745f3039c00c6876
arsenie-wallo <Arsenie.Sarmiento@wallopay.com>
2025-01-13 14:54:55 -0800

    change start level from employee to department

--- challenge/challenge-frontend/src/app/services/data.service.ts
+++ challenge/challenge-frontend/src/app/services/data.service.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@angular/core';
 
 export interface Department {
-  fromName: string;
-  subject: string;
-  date: string;
   id: number;
-  read: boolean,
-  age: 21;
+  name: string;
+  lineManager: string;
 }
 
 @Injectable({
@@ -15,68 +12,44 @@ export interface Department {
 export class DataService {
   public departments: Department[] = [
     {
-      fromName: 'Matt Chorsey',
-      subject: 'New event: Trip to Vegas',
-      date: '9:32 AM',
       id: 0,
-      read: false,
-      age: 21
+      name: "DPT-232",
+      lineManager: "",
     },
     {
-      fromName: 'Lauren Ruthford',
-      subject: 'Long time no chat',
-      date: '6:12 AM',
       id: 1,
-      read: false,
-      age: 21
+      name: "DPT-232",
+      lineManager: "",
     },
     {
-      fromName: 'Jordan Firth',
-      subject: 'Report Results',
-      date: '4:55 AM',
       id: 2,
-      read: false,
-      age: 21
+      name: "",
+      lineManager: "",
     },
     {
-      fromName: 'Bill Thomas',
-      subject: 'The situation',
-      date: 'Yesterday',
       id: 3,
-      read: false,
-      age: 21
+      name: "",
+      lineManager: "",
     },
     {
-      fromName: 'Joanne Pollan',
-      subject: 'Updated invitation: Swim lessons',
-      date: 'Yesterday',
       id: 4,
-      read: false,
-      age: 21
+      name: "",
+      lineManager: "",
     },
     {
-      fromName: 'Andrea Cornerston',
-      subject: 'Last minute ask',
-      date: 'Yesterday',
       id: 5,
-      read: false,
-      age: 21
+      name: "",
+      lineManager: "",
     },
     {
-      fromName: 'Moe Chamont',
-      subject: 'Family Calendar - Version 1',
-      date: 'Last Week',
       id: 6,
-      read: false,
-      age: 21
+      name: "",
+      lineManager: "",
     },
     {
-      fromName: 'Kelly Richardson',
-      subject: 'Placeholder Headhots',
-      date: 'Last Week',
       id: 7,
-      read: false,
-      age: 21
+      name: "",
+      lineManager: "",
     }
   ];
 

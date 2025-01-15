563301c16a85ffa6214ad431fa274d9a0e485010
arsenie-wallo <Arsenie.Sarmiento@wallopay.com>
2025-01-15 14:40:58 -0800

    deleted irrelevant directory: challenge-wallopay

--- challenge-wallo/wallopay-frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'wallopay-frontend',
-  webDir: 'www'
-};
-
-export default config;

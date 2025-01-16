faba13e9ac17f8d80ffb76175669c6a88d7e7af3
arsenie-wallo <Arsenie.Sarmiento@wallopay.com>
2025-01-15 16:26:14 -0800

    tidy up directory

--- challenge/challenge-frontend/src/app/app.routes.ts
+++ challenge/challenge-frontend/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
-// import { NgModule } from '@angular/core';
-// import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AppRoutes } from './app-routes.enum';
+// import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
@@ -27,5 +26,40 @@ export const routes: Routes = [
     path: AppRoutes.DEPARTMENT,
     loadComponent: () => import('./pages/department/department.page').then( m => m.DepartmentPage)
   },
+//   {
+//     path: `department/:id`,
+//     loadChildren: () =>
+//       import(`./department/department-details.module`).then(
+//         (module) => module.DepartmentDetailsPageModule
+//       ),
+//   },
+//   {
+//     path: AppRoutes.ADD_DEPARTMENT,
+//     loadChildren: () => import(`./pages/department/add-department.module`).then( m => m.AddDepartmentPageModule)
+//   },
+//   {
+//     path: AppRoutes.EDIT_DEPARTMENT,
+//     loadChildren: () => import(`./pages/department/edit-department.module`).then( m => m.EditDepartmentPageModule)
+//   },
+//   {
+//     path: AppRoutes.EMPLOYEES,
+//     loadChildren: () => import(`./pages/employee/employee.module`).then( m => m.EmployeePageModule)
+//     // loadChildren: () => import(`./pages/splash-screen/splash-screen.module`).then( m => m.SplashScreenPageModule)
+//   },
+//   {
+//     path: `employee/:id`,
+//     loadChildren: () =>
+//       import(`./employee/employee-details.module`).then(
+//         (module) => module.EmployeeDetailsPageModule
+//       ),
+//   },
+//   {
+//     path: AppRoutes.ADD_EMPLOYEE,
+//     loadChildren: () => import(`./pages/employee/add-employee.module`).then( m => m.AddEmployeePageModule)
+//   },
+//   {
+//     path: AppRoutes.EDIT_EMPLOYEE,
+//     loadChildren: () => import(`./pages/employee/edit-employee.module`).then( m => m.EditEmployeePageModule)
+//   },
 ];
 

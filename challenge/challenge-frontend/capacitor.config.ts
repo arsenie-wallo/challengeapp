37b23bdb068acdcaa7da354f9cfada7344f74d06
arsenie-wallo <arsenie.sarmiento@wallopay.com>
2025-04-10 00:34:13 -0700

    new address api keys

--- challenge/challenge-frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'challenge-frontend',
-  webDir: 'www'
-};
-
-export default config;
